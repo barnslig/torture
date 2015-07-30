528eeb8c3aac6f646fecd9e847e96a20bfec1bac
Leonard Techel <leonard@techel.net>
2015-07-30 20:06:08 +0200

    Frontend: Add some extra documentation to the code (related to #13)
    + Fix HTTP handler error catching
    + More general return format approach
    + Keep helper functions outside of the Search struct space
    + Move json.RawMessage unmarshaling into an own helper function

--- frontend/search.go
+++ frontend/search.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/dustin/go-humanize"
 	"github.com/flosch/pongo2"
-	"log"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -47,26 +47,34 @@ func CreateSearch(cfg SearchConfig) (search *Search, err error) {
 func (search *Search) Handler(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
+	// Catch errors in the following code, log them and return a HTTP 500
 	defer func() {
-		if err, ok := recover().(error); ok {
-			log.Println(err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		if err := recover(); err != nil {
+			search.cfg.Frontend.Log.Println(err)
+			http.Error(w, fmt.Sprintf("%s", err), http.StatusInternalServerError)
 		}
 	}()
 
+	/* Parse GET parameters
+	 * q: Search query
+	 * f: Format. Default is HTML, currently supported options: "json"
+	 * p: Current page. Is zero if not a number
+	 */
 	query := r.FormValue("q")
+	format := r.FormValue("f")
 	page, err := strconv.Atoi(r.FormValue("p"))
 	if err != nil {
 		page = 0
 	}
 
+	// Do the actual search
 	resp, err := search.cfg.Frontend.elasticSearch.Search(query, search.cfg.Frontend.cfg.PerPage, page)
 	if err != nil {
 		search.cfg.Frontend.Log.Panic(err)
 	}
 
-	// API like a bauss
-	if r.FormValue("f") == "json" {
+	// Format: JSON
+	if format == "json" {
 		output, err := json.Marshal(resp.Hits)
 		if err != nil {
 			search.cfg.Frontend.Log.Panic(err)
@@ -81,19 +89,17 @@ func (search *Search) Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Format: HTML (default)
 	var results []Result
 	for _, qr := range resp.Hits.Hits {
-		raw, err := qr.Source.MarshalJSON()
-		if err != nil {
-			search.cfg.Frontend.Log.Panic(err)
-		}
-
+		// Parse the search result into a Result struct
 		var result Result
-		err = json.Unmarshal(raw, &result)
+		err := unmarshalRawJson(qr.Source, &result)
 		if err != nil {
 			search.cfg.Frontend.Log.Panic(err)
 		}
 
+		// Humanize the file size
 		result.HumanSize = humanize.Bytes(result.Size)
 		results = append(results, result)
 	}
@@ -104,8 +110,8 @@ func (search *Search) Handler(w http.ResponseWriter, r *http.Request) {
 		"page":     page,
 		"frompage": search.cfg.Frontend.cfg.PerPage * page,
 		"maxpages": resp.Hits.Total / search.cfg.Frontend.cfg.PerPage,
-		"prevpage": search.getPageLink(page-1, r.URL),
-		"nextpage": search.getPageLink(page+1, r.URL),
+		"prevpage": getPageLink(page-1, r.URL),
+		"nextpage": getPageLink(page+1, r.URL),
 
 		"elapsed":  time.Since(start) / time.Millisecond,
 		"response": resp,
@@ -113,7 +119,7 @@ func (search *Search) Handler(w http.ResponseWriter, r *http.Request) {
 	}, w)
 }
 
-func (search *Search) getPageLink(page int, inURL *url.URL) (outURL string) {
+func getPageLink(page int, inURL *url.URL) (outURL string) {
 	if page < 1 {
 		page = 0
 	}
@@ -123,6 +129,20 @@ func (search *Search) getPageLink(page int, inURL *url.URL) (outURL string) {
 	inURL.RawQuery = qry.Encode()
 
 	outURL = inURL.String()
+
+	return
+}
+
+func unmarshalRawJson(input *json.RawMessage, output interface{}) (err error) {
+	raw, err := input.MarshalJSON()
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(raw, &output)
+	if err != nil {
+		return
+	}
 
 	return
 }
